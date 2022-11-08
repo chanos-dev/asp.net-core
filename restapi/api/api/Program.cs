68ce5cb327734b27fb9555d74755ebbacdc07722
chanos-dev <chanos.dev@gmail.com>
2022-11-09 02:41:34 +0900

    error model binding - custom response message

--- restapi/api/api/Program.cs
+++ restapi/api/api/Program.cs
@@ -1,4 +1,5 @@
 using api.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,18 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
+});
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        return new BadRequestObjectResult(new
+        {
+            Errors = context.ModelState.Select(m => $"{m.Key} : {string.Join(Environment.NewLine, m.Value.Errors.Select(v => v.ErrorMessage))}"),
+            TraceID = Guid.NewGuid().ToString()
+        });
+    };
 });
 
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
