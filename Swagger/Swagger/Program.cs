abc14b7987617457fe32fa4f25531cdd337a2113
chanos-dev <chanos.dev@gmail.com>
2022-11-23 02:35:23 +0900

    - default swagger

---
+++ Swagger/Swagger/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "swagger title",
+        Description = "swagger description",
+        Version = "v1",
+        TermsOfService = new Uri("https://github.com/"),    
+    });
+    
+    
+    
+}).AddSwaggerGenNewtonsoftSupport();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
